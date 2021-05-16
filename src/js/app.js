8f7ff2d2d62dc23d165aeb7cd6a4b5028393b5d6
r0drig <andralmeida0@gmail.com>
2021-05-16 17:23:09 +0100

    Share template correction between mobile and desktop views

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,10 @@
-const btnShare = document.querySelectorAll("#btn-share1, #btn-share2");
+const btnShare = document.querySelector("button");
+const articleFooter = document.querySelector(".article-footer");
 const articleFooterAuthor = document.querySelector(".article-footer-author");
 const articleFooterShare = document.querySelector(".article-footer-share");
-const articleFooterTooltip = document.querySelector(".article-footer-tooltip");
 
-const laptopSize = 1024;
-
-var vw = null;
-
-btnShare.forEach(btn => {
-   btn.addEventListener('click', event =>{
-      if ( vw < laptopSize ){
-         articleFooterAuthor.classList.toggle('hide-mobile');
-         articleFooterShare.classList.toggle('hide-mobile');
-      } else {
-         articleFooterTooltip.classList.toggle('hide-desktop');
-         btn.classList.toggle('btn-share-open');
-         btn.children[0].classList.toggle('svg-background');
-      }
-   });
+btnShare.addEventListener('click', event =>{
+   articleFooter.classList.toggle('share-on');
+   articleFooterAuthor.classList.toggle('hide');
+   articleFooterShare.classList.toggle('hide');
 });
-
-function updateVw(){
-   vw = Math.max(window.innerWidth || 0, document.documentElement.clientWidth || 0);
-}
